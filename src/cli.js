fd143268876c5ac6028d7a26ff2e95aa0ac94056
Denis Sikuler <mforeg@yandex.ru>
2014-04-27 00:08:10 +0400

    cli.js refactoring

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@
  * @module cli
  */
 
-/*jshint boss:true, latedef:false, laxbreak:true*/
+/*jshint boss:true, laxbreak:true*/
 
 
 function resourceListToString(resourceList, short) {
@@ -30,6 +30,111 @@ function resourceListToString(resourceList, short) {
     }
     return result.join("");
 }
+
+
+var fs = require("fs"),
+    
+    charm = require("charm")(),
+    nomnom = require("nomnom"),
+    
+    resourceUnit = require("./resource"),
+    seeq = require("./seeq"),
+    
+    pkg = JSON.parse(fs.readFileSync(__dirname + "/../package.json", "utf8")),
+    resourceList = resourceUnit.getList({includeApi: true}),
+    generalSettingName = {
+        "partialMatch": null, 
+        "caseSensitive": null, 
+        "search": null, 
+        "limit": null
+    },
+    
+    argParser = nomnom()
+                    .script(pkg.name)
+                    .options({
+                        "name": {
+                            position: 0,
+                            list: true,
+                            help: "Name/string that should be searched for or checked for presence on a resource"
+                        },
+                        "help": {
+                            abbr: "h",
+                            help: "Show usage information and exit"
+                        },
+                        "listResource": {
+                            abbr: "l",
+                            full: "list-resource",
+                            flag: true,
+                            help: "Show information about all available resources"
+                        },
+                        "resource": {
+                            abbr: "r",
+                            full: "at",
+                            help: "Filter for available resources by name: comma-separated list of names (case-insensitive) of resources that should be checked/searched"
+                        },
+                        "resourceTag": {
+                            abbr: "t",
+                            full: "tag",
+                            help: "Filter for available resources by tag: comma-separated list of tags (case-insensitive); -tag means that resource should not have the tag"
+                        },
+                        "checkAllTags": {
+                            full: "all-tag",
+                            flag: true,
+                            help: "Whether all specified tags should be checked for a resource"
+                        },
+                        "partialMatch": {
+                            abbr: "p",
+                            full: "partial-match",
+                            help: "Allow partial matching when checking name: 0 - disallow (by default), 1 - allow at the beginning of matching strings, 2 - allow substring matching",
+                            choices: [0, 1, 2]
+                        },
+                        "caseSensitive": {
+                            abbr: "c",
+                            full: "case-sensitive",
+                            flag: true,
+                            help: "Use case-sensitive check/search when possible"
+                        },
+                        "search": {
+                            abbr: "s",
+                            flag: true,
+                            help: "Make search instead of check"
+                        },
+                        "limit": {
+                            abbr: "m",
+                            help: "Limit of quantity of results per resource",
+                            callback: function(value) {
+                                if (value < 1) {
+                                    return "Limit should be equal to or greater than 1";
+                                }
+                            }
+                        },
+                        "verbose": {
+                            abbr: "V",
+                            flag: true,
+                            help: "Enable verbose output"
+                        },
+                        "version": {
+                            abbr: "v",
+                            flag: true,
+                            help: "Show program version"
+                        }
+                    })
+                    .help(resourceListToString(resourceList, true));
+
+
+(function() {
+    var nI, nL, settings, sName, value;
+    // Add options of resources
+    for (nI = 0, nL = resourceList.length; nI < nL; nI++) {
+        value = resourceList[nI];
+        if (settings = value.api.settings) {
+            sName = value.id;
+            for (value in settings) {
+                argParser.option(sName + "-" + value, settings[value]);
+            }
+        }
+    }
+}());
 
 
 function showProgress(data) {
@@ -61,7 +166,7 @@ function licenseToString(license) {
 }
 
 
-function showResult(resultMap) {
+function showResult(resultMap, nameList, args) {
     /*jshint expr:true*/
     var bVerbose = args.verbose,
         out = ["\nResults:"], 
@@ -125,196 +230,159 @@ function showResult(resultMap) {
 }
 
 
-var fs = require("fs"),
+/**
+ * Perform operation using specified options.
+ * 
+ * @param {Object} [args]
+ *      Operation options.
+ * @alias module:cli.run
+ */
+function run(args) {
+    var bShowUsage = true,
+        list,
+        nameList,
+        nL,
+        resourceNameList,
+        resourceSettings,
+        sName,
+        value;
     
-    charm = require("charm")(),
-    nomnom = require("nomnom"),
+    if (! args) {
+        args = {};
+    }
     
-    resourceUnit = require("./resource"),
-    seeq = require("./seeq"),
+    if (args.version) {
+        console.log("Version: " + pkg.version);
+        bShowUsage = false;
+    }
     
-    bShowUsage = true,
-    pkg = JSON.parse(fs.readFileSync(__dirname + "/../package.json", "utf8")),
-    resourceList = resourceUnit.getList({includeApi: true}),
-    generalSettingName = {
-        "partialMatch": null, 
-        "caseSensitive": null, 
-        "search": null, 
-        "limit": null
-    },
-    nameList,
-    resourceNameList,
-    resourceSettings,
-    list,
-    nI,
-    nL,
-    sName,
-    value,
+    if (args.listResource) {
+        console.log(resourceListToString(resourceList));
+        bShowUsage = false;
+    }
     
-    argParser = nomnom(),
-    args = argParser
-                .script(pkg.name)
-                .options({
-                    "name": {
-                        position: 0,
-                        list: true,
-                        help: "Name/string that should be searched for or checked for presence on a resource"
-                    },
-                    "help": {
-                        abbr: "h",
-                        help: "Show usage information and exit"
-                    },
-                    "listResource": {
-                        abbr: "l",
-                        full: "list-resource",
-                        flag: true,
-                        help: "Show information about all available resources"
-                    },
-                    "resource": {
-                        abbr: "r",
-                        full: "at",
-                        help: "Filter for available resources by name: comma-separated list of names (case-insensitive) of resources that should be checked/searched"
-                    },
-                    "resourceTag": {
-                        abbr: "t",
-                        full: "tag",
-                        help: "Filter for available resources by tag: comma-separated list of tags (case-insensitive); -tag means that resource should not have the tag"
-                    },
-                    "checkAllTags": {
-                        full: "all-tag",
-                        flag: true,
-                        help: "Whether all specified tags should be checked for a resource"
-                    },
-                    "partialMatch": {
-                        abbr: "p",
-                        full: "partial-match",
-                        help: "Allow partial matching when checking name: 0 - disallow (by default), 1 - allow at the beginning of matching strings, 2 - allow substring matching",
-                        choices: [0, 1, 2]
-                    },
-                    "caseSensitive": {
-                        abbr: "c",
-                        full: "case-sensitive",
-                        flag: true,
-                        help: "Use case-sensitive check/search when possible"
-                    },
-                    "search": {
-                        abbr: "s",
-                        flag: true,
-                        help: "Make search instead of check"
-                    },
-                    "limit": {
-                        abbr: "m",
-                        help: "Limit of quantity of results per resource",
-                        callback: function(value) {
-                            if (value < 1) {
-                                return "Limit should be equal to or greater than 1";
-                            }
-                        }
-                    },
-                    "verbose": {
-                        abbr: "V",
-                        flag: true,
-                        help: "Enable verbose output"
-                    },
-                    "version": {
-                        abbr: "v",
-                        flag: true,
-                        help: "Show program version"
+    if (args.name && args.name.length) {
+        // List of names to check
+        nameList = args.name;
+        // Determine resources to check
+        if (args.resource) {
+            args.resource = args.resource.split(",");
+        }
+        if (args.resourceTag) {
+            args.resourceTag = args.resourceTag.split(",");
+        }
+        resourceNameList = resourceUnit.getNameList({
+                                                        selectName: args.resource, 
+                                                        selectTag: args.resourceTag,
+                                                        checkAllTags: args.checkAllTags
+                                                    });
+        // Make check if resources are specified
+        if (resourceNameList.length) {
+            // Prepare resources settings
+            resourceSettings = {
+            };
+            for (sName in args) {
+                value = args[sName];
+                // General settings
+                if (sName in generalSettingName) {
+                    (resourceSettings._general || (resourceSettings._general = {}))[sName] = value;
+                }
+                // Resource-specific settings
+                else if (list = /^(\w+)\-(.+)$/.exec(sName)) {
+                    if (sName = resourceUnit.getIdByName(list[1])) {
+                        ( resourceSettings[sName] || (resourceSettings[sName] = {}) )[ list[2] ] = value;
                     }
-                })
-                .help(resourceListToString(resourceList, true));
-
-
-// Add options of resources
-for (nI = 0, nL = resourceList.length; nI < nL; nI++) {
-    value = resourceList[nI];
-    if (list = value.api.settings) {
-        sName = value.id;
-        for (value in list) {
-            args.option(sName + "-" + value, list[value]);
+                }
+            }
+            // Start checking
+            console.log("Checking " + resourceNameList.join(", ") + "...");
+            if (nameList.length > 1 || resourceNameList.length > 1) {
+                value = showProgress;
+                charm.pipe(process.stdout);
+                charm.write("Progress: 0 %");
+            }
+            else {
+                value = null;
+            }
+            seeq.search(nameList, 
+                        function(resultMap) {
+                            showResult(resultMap, nameList, args);
+                        },
+                        {
+                            resource: resourceNameList, 
+                            settings: resourceSettings,
+                            progressCallback: value
+                        });
+        }
+        // If no resource is found
+        else {
+            console.log("No resource is found.");
+            if (args.resource) {
+                list = args.resource.filter(function(name) {
+                    return ! resourceUnit.isAvailable(name);
+                });
+                if (nL = list.length) {
+                    console.log(nL > 1
+                                    ? "The following resources are unknown: " + list.join(", ")
+                                    : list[0] + " is unknown resource");
+                }
+            }
+            console.log("Run `" + pkg.name + " -l` to see information about all available resources.");
         }
     }
-}
-
-// Parse arguments
-args = args.parse();
-
-if (args.version) {
-    console.log("Version: " + pkg.version);
-    bShowUsage = false;
-}
-
-if (args.listResource) {
-    console.log(resourceListToString(resourceList));
-    bShowUsage = false;
-}
-
-if (args.name && args.name.length) {
-    // List of names to check
-    nameList = args.name;
-    // Determine resources to check
-    if (args.resource) {
-        args.resource = args.resource.split(",");
+    else if (bShowUsage) {
+        argParser.print(argParser.getUsage());
     }
-    if (args.resourceTag) {
-        args.resourceTag = args.resourceTag.split(",");
-    }
-    resourceNameList = resourceUnit.getNameList({
-                                                    selectName: args.resource, 
-                                                    selectTag: args.resourceTag,
-                                                    checkAllTags: args.checkAllTags
-                                                });
-    // Make check if resources are specified
-    if (resourceNameList.length) {
-        // Prepare resources settings
-        resourceSettings = {
-        };
-        for (sName in args) {
-            value = args[sName];
-            // General settings
-            if (sName in generalSettingName) {
-                (resourceSettings._general || (resourceSettings._general = {}))[sName] = value;
-            }
-            // Resource-specific settings
-            else if (list = /^(\w+)\-(.+)$/.exec(sName)) {
-                if (sName = resourceUnit.getIdByName(list[1])) {
-                    ( resourceSettings[sName] || (resourceSettings[sName] = {}) )[ list[2] ] = value;
+}
+
+
+/**
+ * Perform operation specified via command-line arguments.
+ * 
+ * @param {Object} [options]
+ *      Predefined values for command-line options.
+ *      Each option specified in this object will be used only when the corresponding option is not set in command-line arguments.
+ * @alias module:cli.cli
+ * @see {@link module:cli.run run}
+ */
+function cli(options) {
+    var args = argParser.parse(),
+        optionMap,
+        sName,
+        value;
+    
+    // Use predefined values for absent arguments
+    if (options) {
+        optionMap = argParser.specs;
+        for (sName in optionMap) {
+            if (! (sName in args)) {
+                if (sName in options) {
+                    args[sName] = options[sName];
+                }
+                else {
+                    value = optionMap[sName];
+                    if (value.full in options) {
+                        args[sName] = options[value.full];
+                    }
+                    else if (value.abbr in options) {
+                        args[sName] = options[value.abbr];
+                    }
                 }
             }
         }
-        // Start checking
-        console.log("Checking " + resourceNameList.join(", ") + "...");
-        if (nameList.length > 1 || resourceNameList.length > 1) {
-            value = showProgress;
-            charm.pipe(process.stdout);
-            charm.write("Progress: 0 %");
-        }
-        else {
-            value = null;
-        }
-        seeq.search(nameList, showResult, 
-                    {
-                        resource: resourceNameList, 
-                        settings: resourceSettings,
-                        progressCallback: value
-                    });
     }
-    // If no resource is found
-    else {
-        console.log("No resource is found.");
-        if (args.resource) {
-            list = args.resource.filter(function(name) {
-                return ! resourceUnit.isAvailable(name);
-            });
-            if (nI = list.length) {
-                console.log(nI > 1
-                                ? "The following resources are unknown: " + list.join(", ")
-                                : list[0] + " is unknown resource");
-            }
-        }
-        console.log("Run `" + pkg.name + " -l` to see information about all available resources.");
-    }
+    // Run CLI
+    run(args);
 }
-else if (bShowUsage) {
-    argParser.print(argParser.getUsage());
+
+
+// Exports
+
+exports.cli = cli;
+exports.run = run;
+
+
+// If script is run directly from Node
+if (require.main === module) {
+    cli();
 }
